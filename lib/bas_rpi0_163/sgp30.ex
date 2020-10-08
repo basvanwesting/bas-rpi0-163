7f502d3b681789b43e65e475d836b5b93c2ed8d9
basvanwesting <basvanwesting@hotmail.com>
2020-10-08 12:04:29 +0200

    Add SGP30 sensor

---
+++ lib/bas_rpi0_163/sgp30.ex
@@ -0,0 +1,58 @@
+defmodule BasRpi0163.SGP30 do
+  @moduledoc false
+
+  use GenServer
+  #require Logger
+
+  @i2c Circuits.I2C
+  @i2c_bus "i2c-1"
+  @i2c_retry_count 2
+
+  @address 0x58
+
+  @init_air_quality_cmd         <<0x2003::16>>
+  @init_air_quality_delay       5000
+  @measure_air_quality_cmd      <<0x2008::16>>
+  @measure_air_quality_bytes    6
+  @measure_air_quality_duration 12
+  @measure_air_quality_interval 1000
+
+  defmodule State do
+    @moduledoc false
+    defstruct i2c_ref: nil, eco2_ppm: nil, tvoc_ppb: nil
+  end
+
+  def start_link(_args) do
+    GenServer.start_link(__MODULE__, nil, name: __MODULE__)
+  end
+
+  def init(_args) do
+    {:ok, %State{}, {:continue, :connect}}
+  end
+
+  def handle_continue(:connect, state) do
+    with {:ok, i2c_ref} <- @i2c.open(@i2c_bus) do
+      Process.send_after(self(), :init_air_quality, @init_air_quality_delay)
+      {:noreply, %{state | i2c_ref: i2c_ref}}
+    end
+  end
+
+  def handle_info(:init_air_quality, state) do
+    with :ok <- @i2c.write(state.i2c_ref, @address, @init_air_quality_cmd, retries: @i2c_retry_count) do
+      Process.send_after(self(), :measure_air_quality, @measure_air_quality_interval)
+      {:noreply, state}
+    end
+  end
+
+  def handle_info(:measure_air_quality, state) do
+    with :ok <- @i2c.write(state.i2c_ref, @address, @measure_air_quality_cmd, retries: @i2c_retry_count),
+         :ok <- :timer.sleep(@measure_air_quality_duration),
+         {:ok, reply} <- @i2c.read(state.i2c_ref, @address, @measure_air_quality_bytes, retries: @i2c_retry_count) do
+
+       <<eco2_ppm::16, _eco2_ppm_crc::8, tvoc_ppb::16, _tvoc_ppb_crc::8>> = reply
+       Process.send_after(self(), :measure_air_quality, @measure_air_quality_interval - @measure_air_quality_duration)
+       {:noreply, %{state | eco2_ppm: eco2_ppm, tvoc_ppb: tvoc_ppb}}
+    end
+  end
+
+end
---
+++ workspace_sgp30.exs
@@ -0,0 +1,84 @@
+
+i2c = Circuits.I2C
+i2c_retry_count = 2
+{:ok, ref} = i2c.open("i2c-1")
+
+address = 0x58
+
+init_air_quality        = 0x2003
+measure_air_quality     = 0x2008
+get_baseline            = 0x2015
+set_baseline            = 0x201e
+set_humidity            = 0x2061
+measure_test            = 0x2032
+get_feature_set_version = 0x202f
+measure_raw_signals     = 0x2050
+
+crc_options = %{
+  width: 8,
+  poly: 0x31,
+  init: 0xff,
+  refin: false,
+  refout: false,
+  xorout: 0x00
+}
+
+0x92 = CRC.calculate(<<0xBEEF::16>>, crc_options)
+
+
+# Get feature set
+
+message = <<get_feature_set_version::16>>
+reply = i2c.write(ref, address, message, retries: i2c_retry_count)
+bytes_to_read = 3
+reply = i2c.read(ref, address, bytes_to_read, retries: i2c_retry_count)
+{:ok, <<product_type::4, _::4, product_version::8, crc::8>>} = reply
+
+
+# Measure air quality
+
+message = <<measure_air_quality::16>>
+reply = i2c.write(ref, address, message, retries: i2c_retry_count)
+bytes_to_read = 6
+reply = i2c.read(ref, address, bytes_to_read, retries: i2c_retry_count)
+{:ok, <<eco2_ppm::16, eco2_ppm_crc::8, tvoc_ppb::16, tvoc_ppb_crc::8>>} = reply
+
+
+# self test
+
+message = <<measure_test::16>>
+reply = i2c.write(ref, address, message, retries: i2c_retry_count)
+bytes_to_read = 3
+reply = i2c.read(ref, address, bytes_to_read, retries: i2c_retry_count)
+{:ok, <<data::16, crc::8>>} = reply
+
+data == 0xD400
+crc == CRC.calculate(<<0xD400::16>>, crc_options)
+
+# loop
+
+message = <<init_air_quality::16>>
+reply = i2c.write(ref, address, message, retries: i2c_retry_count)
+
+for i <- 1..1000 do
+  message = <<measure_air_quality::16>>
+  reply = i2c.write(ref, address, message, retries: i2c_retry_count)
+
+  :timer.sleep(50)
+
+  bytes_to_read = 6
+  reply = i2c.read(ref, address, bytes_to_read, retries: i2c_retry_count)
+  {:ok, <<eco2_ppm::16, eco2_ppm_crc::8, tvoc_ppb::16, tvoc_ppb_crc::8>>} = reply
+
+  IO.puts "eCO2: #{eco2_ppm} ppm, TVOC: #{tvoc_ppb} ppb"
+
+  :timer.sleep(950)
+end
+
+
+
+
+
+
+
+
